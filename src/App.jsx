454a86c35700ad6b10e516c5df04b3a70ff4ed5d
praks0208 <praks.sawant26@gmail.com>
2024-05-22 23:29:51 +0530

    Add Rematch Option

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -63,13 +63,19 @@ const hasDraw = gameTurns.length === 9 && !winner;
       return updatedTurns;
     });
   }
+
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
         <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
         <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
       </ol>
-      {(winner || hasDraw) && <GameOver winner={winner} /> }
+      {(winner || hasDraw) && (
+      <GameOver winner={winner} onRestart={handleRestart}/> )}
       <GameBoard onSelectSquare={handleSelectSquare}
         turns={gameTurns} board={gameBoard} />
     </div>
