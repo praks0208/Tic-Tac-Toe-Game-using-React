7c30f95a0cf0c9f1643722046f85ad4adee9cbf0
praks0208 <praks.sawant26@gmail.com>
2024-05-22 20:06:44 +0530

    Derive State From Props

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setGameTurns(prevTurns =>{
       let currentPlayer = 'X';
 
-      if(prevTurns,length>0 && prevTurns[0].player === 'X'){
+      if(prevTurns.length>0 && prevTurns[0].player === 'X'){
         currentPlayer = 'O';
       }
 
@@ -28,7 +28,8 @@ function App() {
         <Player initialName="Player 1"  symbol="X" isActive={activePlayer === 'X'} />
         <Player initialName="Player 2"  symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      <GameBoard onSelectSquare={handleSelectSquare}
+      turns={gameTurns} />
     </div>
     <Log />
   </main>;
